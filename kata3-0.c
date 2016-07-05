c8535db166e1e81325b8790340d7696c7c4eb737
Paul Buis <00pebuis@bsu.edu>
2016-07-05 12:39:27 -0400

    Initial Commit

---
+++ kata3-0.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+double factorial(unsigned int n) {
+    if (n == 0) {
+       return 1.0;
+    }
+    return n*factorial(n-1);
+}
+
+
+int main(int argc, char** argv) {
+    printf(" 10!= %20g\n", factorial(10));
+    printf(" 20!= %20g\n", factorial(20));
+    printf(" 40!= %20g\n", factorial(40));
+    printf(" 80!= %20g\n", factorial(80));
+    printf("160!= %20g\n", factorial(160));
+    printf("200!= %20g\n", factorial(200));
+    return 0;
+}
